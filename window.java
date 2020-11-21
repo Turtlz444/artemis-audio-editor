578bf22518d7343eb3b2d922c47f6d1236d8ff55
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-11-21 13:01:54 -0600

    expiremented with adding text

--- window.java
+++ window.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 class Main {
     public static void main(String[] args) {
@@ -11,5 +14,13 @@ class Main {
         frame.setTitle("Artemis DAW");
         frame.getContentPane().setBackground(Color.decode("#151515"));
         frame.setIconImage(img.getImage());
+        JLabel title = new JLabel("My Label");
+        Font titleFont = new Font("PTSans-Regular.ttf", Font.PLAIN, 28);
+        title.setText("<html>hi</html>");
+        title.setFont(titleFont);
+        title.setForeground(Color.white);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setVerticalAlignment(SwingConstants.TOP);
+        frame.add(title);
     }
 }
