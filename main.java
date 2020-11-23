1b323bd8e73bcc91728f186046e64a10d9a1d145
Heccing Commie <51051125+Turtlz444@users.noreply.github.com>
2020-11-23 13:15:55 -0600

    font setup

--- main.java
+++ main.java
@@ -22,20 +22,23 @@ class Main {
 //window setup
         ImageIcon img = new ImageIcon("Artemis DAW.png");
         JFrame frame = new JFrame("DAW Frame");
-        frame.setSize(800, 600);
+        frame.setSize(800, 800);
         frame.setVisible(true);
-        frame.setTitle("Artemis DAW");
+        frame.setTitle("Artemis Audio Editor");
         frame.getContentPane().setBackground(Color.decode("#151515"));
         frame.setIconImage(img.getImage());
 
 //styling setup
-        Font main = new Font("PTSans-Regular.ttf", Font.PLAIN, 18);
-        Font sub = new Font("AzoSans-Regular.ttf", Font.PLAIN, 12);
+        Font prim = new Font("PTSans-Regular.ttf", Font.BOLD, 18); //Headers
+        Font sec = new Font("AzoSans-Italic.ttf", Font.ITALIC, 18); //Subheaders
+        Font tert = new Font("AzoSans-Regular.ttf", Font.PLAIN, 12); //Regular text
 
 //header setup
         Box tabs = Box.createHorizontalBox();
-        tabs.add(new JButton("File")).setFont(sub);
-        tabs.add(new JButton("Edit")).setFont(sub);
+        tabs.add(new JButton("File")).setFont(tert);
+        tabs.add(new JButton("Edit")).setFont(tert);
+        tabs.add(new JButton("View")).setFont(tert);
+        tabs.add(new JButton("Help")).setFont(tert);
         tabs.setBackground(Color.WHITE);
         frame.add(tabs, BorderLayout.BEFORE_FIRST_LINE);
     }
